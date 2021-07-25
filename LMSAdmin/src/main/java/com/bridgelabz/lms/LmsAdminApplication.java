913f0cde47850814f3ab1ad3f9c2f92b2ae3e9a8
siva209 <sivanarayana209@gmail.com>
2021-07-25 06:02:35 +0530

    Added:Welcome to LMS Application

---
+++ LMSAdmin/src/main/java/com/bridgelabz/lms/LmsAdminApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsAdminApplication.class, args);
+		System.out.println("welcome to lms");
+	}
+
+}
---
+++ LMSAdmin/src/main/java/com/bridgelabz/lms/dto/LoginDto.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.lms.dto;
+
+public class LoginDto {
+
+}
