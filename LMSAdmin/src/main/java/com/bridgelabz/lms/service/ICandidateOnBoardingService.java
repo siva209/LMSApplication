19ab97cb09093562500a31f7533018d4c2711a67
siva209 <sivanarayana209@gmail.com>
2021-08-06 14:41:55 +0530

    Refactored:Changes are done

---
+++ LMSAdmin/src/main/java/com/bridgelabz/lms/service/ICandidateOnBoardingService.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.lms.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.lms.dto.CandidatEOnBoardUpdateDTO;
+import com.bridgelabz.lms.dto.CandidatEOnBoardingDTO;
+import com.bridgelabz.lms.dto.CandidateHiredDTO;
+import com.bridgelabz.lms.dto.UpdateHiringDto;
+import com.bridgelabz.lms.model.HiringCandidate;
+import com.bridgelabz.lms.model.CandidateOnboardingDetails;
+import com.bridgelabz.lms.response.Response;
+@Service
+public interface ICandidateOnBoardingService {
+	 public Response createUser(String token,CandidatEOnBoardingDTO dto);
+	 public Response getAllOnBoardingcandidates(String token);
+	 public Response getOnBoardCandidate(String token,Long id);
+	 public Response updateOnBoardingCandidate(String token,Long id,CandidatEOnBoardUpdateDTO dto);
+	 public void deleteOnBoardingCandidateById(String token,Long id) ;
+	 public Response updateStatus(String token, Long id, String keyText);
+	 public  Response getCount(String token);
+	    
+}
