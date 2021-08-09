3e3b4efa093ef986eeeba6ba2567db4375d92a9f
siva209 <sivanarayana209@gmail.com>
2021-08-09 13:43:47 +0530

    Refactored:Registered with eureka server

--- LMSAdmin/src/main/java/com/bridgelabz/lms/LmsAdminApplication.java
+++ LMSAdmin/src/main/java/com/bridgelabz/lms/LmsAdminApplication.java
@@ -2,8 +2,10 @@ package com.bridgelabz.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LmsAdminApplication {
 
 	public static void main(String[] args) {
